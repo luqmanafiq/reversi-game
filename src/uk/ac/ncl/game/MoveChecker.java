bf7fb080d1d49083325502f30f4950360159564d
USER <luqmanafiq135.la@gmail.com>
2023-05-12 12:47:48 +0100

    it is not cells[i][i] but cells[i][j] = tempEl;

--- src/uk/ac/ncl/game/MoveChecker.java
+++ src/uk/ac/ncl/game/MoveChecker.java
@@ -134,7 +134,7 @@ public class MoveChecker {
         if (grayCells == null)
             return;
         for (Cell cell : grayCells) {
-            cell.setMove(cell.getMove());
+            cell.setMove(null);
         }
     }
 }
