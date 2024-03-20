66e40e30da6fcad479e16342a25b03b678fdbf9e
USER <luqmanafiq135.la@gmail.com>
2024-03-20 00:00:58 +0000

    Changes to cell.java by removing (!)

--- src/uk/ac/ncl/Main.java
+++ src/uk/ac/ncl/Main.java
@@ -8,7 +8,6 @@ package uk.ac.ncl;
 import uk.ac.ncl.ui.MainPanel;
 
 public class Main {
-
     public static void main(String[] args) {
         MainPanel ex = new MainPanel();
         ex.setVisible(true);
